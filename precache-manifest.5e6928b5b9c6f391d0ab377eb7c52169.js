ba763077e4daa0f797c7c0b6c62798f1f8924f80
Ansh Ratnam <ansh@uber.com>
2019-01-05 15:17:34 -0800

    Updates

---
+++ precache-manifest.5e6928b5b9c6f391d0ab377eb7c52169.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6196780433171a1892c7658d3475871d",
+    "url": "/static/media/uber_logo.61967804.jpg"
+  },
+  {
+    "revision": "0f9c390de48eb3904fe6131de860537a",
+    "url": "/static/media/personal_photo.0f9c390d.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "971320584e4c915584e7",
+    "url": "/static/js/main.97132058.chunk.js"
+  },
+  {
+    "revision": "1a596ca731a12bb19d37",
+    "url": "/static/js/1.1a596ca7.chunk.js"
+  },
+  {
+    "revision": "971320584e4c915584e7",
+    "url": "/static/css/main.d8271d72.chunk.css"
+  },
+  {
+    "revision": "75bab7c4adee745e286116bb34f30b2e",
+    "url": "/index.html"
+  }
+];
